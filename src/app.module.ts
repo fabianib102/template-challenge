046b114a84e6970a9fdb4c32a41aa3d0fbde76af
Fabian Ibañez <fabianib102@gmail.com>
2024-05-27 15:36:07 -0300

    Add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     MoviesModule,
-    MongooseModule.forRoot('mongodb+srv://usermongo:bqgZZHBQmXqLnAm5@cluster0.xpas1ok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xpas1ok.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_APPNAME}`)
   ],
   controllers: [],
   providers: [],
